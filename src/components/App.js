d6b453343513a9d97b5c483677f6c4105a997583
Denis Lafontant <icebox827@gmail.com>
2021-05-03 09:57:19 -0400

    Implement the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,12 @@
+import BookList from '../containers/BookList';
+import BooForm from '../containers/BookForm';
 import '../App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
+      <BookList />
+      <BooForm />
     </div>
   );
 }
