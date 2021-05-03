92f1700f482f3e67d61734be996928461b5dd888
Denis Lafontant <icebox827@gmail.com>
2021-05-03 12:15:44 -0400

    Update BookForm and create action.js file

---
+++ src/containers/BookForm.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const BookForm = () => {
+  const category = [
+    'Action',
+    'Biography',
+    'History',
+    'Horror',
+    'Kids',
+    'Learning',
+    'Sci-Fi'
+  ]
+  // const selectedCategory = 
+  return (
+    <div>
+      <form>
+        <fieldset>
+          <legend>Add Book</legend>
+          <label>Book Title</label>
+          <input type='text' id='book' className='book' />
+          <br />
+          <br />
+          <select>
+            <option></option>
+          </select>
+          <button type='submit' value='submit'>
+            Submit
+          </button>
+        </fieldset>
+      </form>
+    </div>
+  )
+}
+
+export default BookForm
