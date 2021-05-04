a895f8aa549b10c8a01fc09acf02869168cf062f
Denis Lafontant <icebox827@gmail.com>
2021-05-04 12:18:58 -0400

    Create NavBar component and import it to App

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+
+const NavBar = () => {
+  <nav className="flex">
+    <div className="navbar flex">
+      <div className="logo">Bookstore CMS</div>
+      <div className="heading selected">Books</div>
+      <div className="heading">Categories</div>
+    </div>
+    <div className="profile">
+      <i className="fa fa-user" />
+    </div>
+  </nav>
+};
+
+export default NavBar;
